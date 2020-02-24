128540722ddf7fe290649280fb5379a5192a0a04
Paul Nadal <paul.nadal@epfl.ch>
2020-02-24 14:57:07 +0100

    Start étape 3

--- src/ch/epfl/rigel/astronomy/Epoch.java
+++ src/ch/epfl/rigel/astronomy/Epoch.java
@@ -1,4 +1,23 @@
 package ch.epfl.rigel.astronomy;
 
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
 public enum Epoch {
+    J2000(ZonedDateTime.of(LocalDate.of(2000, Month.JANUARY, 1), LocalTime.NOON, ZoneOffset.UTC)),
+    J2010(ZonedDateTime.of(LocalDate.of(2010, Month.JANUARY, 1).minusDays(1), LocalTime.MIDNIGHT, ZoneOffset.UTC));
+
+    private ZonedDateTime epoch;
+
+    private Epoch(ZonedDateTime epoch){
+        this.epoch = epoch;
+    }
+
+    public ZonedDateTime getZonedDateTime(){
+        return this.epoch;
+    }
+
+    public double daysUntil(ZonedDateTime when){
+        return (double) getZonedDateTime().until(when, ChronoUnit.MILLIS)/86400000;
+    }
 }
