6f19361d407ac1349db378367636171dc10e0acb
alexandrebrun <alexandre.brun@epfl.ch>
2020-02-24 14:58:02 +0100

    sideralTime class

--- src/ch/epfl/rigel/astronomy/SideralTime.java
+++ src/ch/epfl/rigel/astronomy/SideralTime.java
@@ -1,4 +1,24 @@
 package ch.epfl.rigel.astronomy;
 
-public class SideralTime {
+import ch.epfl.rigel.coordinates.GeographicCoordinates;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+
+public abstract class SideralTime {
+
+    public static double greenwich(ZonedDateTime when){
+        ZonedDateTime greenwich = when;
+        greenwich = when.
+        return when.withZoneSameInstant(ZoneOffset.UTC)
+        )
+    }
+
+    public static double local(ZonedDateTime when, GeographicCoordinates where){
+
+    }
+
+
+
 }
