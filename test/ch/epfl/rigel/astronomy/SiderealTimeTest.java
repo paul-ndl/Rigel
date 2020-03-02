3954d3ae910a92142c502b64405869dab3c45fbc
Paul Nadal <paul.nadal@epfl.ch>
2020-03-02 11:43:24 +0100

    test part 3 (start)

--- test/ch/epfl/rigel/astronomy/SiderealTimeTest.java
+++ test/ch/epfl/rigel/astronomy/SiderealTimeTest.java
@@ -1,6 +1,7 @@
 package ch.epfl.rigel.astronomy;
 
 import ch.epfl.rigel.coordinates.GeographicCoordinates;
+import ch.epfl.rigel.math.Angle;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,14 +15,14 @@ public class SiderealTimeTest {
     @Test
     void greenwichWorksWithKnownValues(){
         //ZonedDateTime a = ZonedDateTime.of();
-        assertEquals(1.2220619247737088,SiderealTime.greenwich(ZonedDateTime.of(1980,4,22,14,36,51,67, ZoneId.of("UTC"))), 10e-10);
+        assertEquals(Angle.ofArcsec(16805.23),SiderealTime.greenwich(ZonedDateTime.of(1980,4,22,14,36,51,(int) 67e7, ZoneId.of("UTC"))));
 
     }
 
     @Test
     void localWorksWithKnownValues(){
         //ZonedDateTime a = ZonedDateTime.of();
-        assertEquals(1.74570958832716,SiderealTime.local(ZonedDateTime.of(1980,4,22,14,36,51,67, ZoneOffset.UTC), GeographicCoordinates.ofDeg(30,45)), 10e-10);
+        //assertEquals(1.74570958832716,SiderealTime.local(ZonedDateTime.of(1980,4,22,14,36,51,67, ZoneOffset.UTC), GeographicCoordinates.ofDeg(30,45)), 10e-10);
 
     }
 }
--- test/ch/epfl/rigel/coordinates/EclipticToEquatorialConversionTest.java
+++ test/ch/epfl/rigel/coordinates/EclipticToEquatorialConversionTest.java
@@ -1,4 +1,18 @@
 package ch.epfl.rigel.coordinates;
 
+import ch.epfl.rigel.math.Angle;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EclipticToEquatorialConversionTest {
+
+    @Test
+    void applyWorksOnKnownValues(){
+        EclipticToEquatorialConversion e = new EclipticToEquatorialConversion(ZonedDateTime.of(2016,7,9,11,31,35, 0, ZoneId.of("UTC")));
+        assertEquals(EquatorialCoordinates.of(Angle.ofHr(0.73106), Angle.ofDeg(11.03858)).toString(), e.apply(EclipticCoordinates.of(Angle.ofDeg(14.4031), Angle.ofDeg(5.82))).toString());
+    }
 }
