39e8126d5d9b8ea27651c592ad4d86c49cf8015e
Paul Nadal <paul.nadal@epfl.ch>
2020-03-03 15:16:51 +0100

    test part 3 (not far from the end)

--- test/ch/epfl/rigel/coordinates/EclipticToEquatorialConversionTest.java
+++ test/ch/epfl/rigel/coordinates/EclipticToEquatorialConversionTest.java
@@ -11,6 +11,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class EclipticToEquatorialConversionTest {
 
     @Test
+    void applyOK1() {
+        EclipticToEquatorialConversion e = new EclipticToEquatorialConversion(ZonedDateTime.of(2020, 8, 14, 11, 0, 0, 0, ZoneId.of("Europe/Paris")));
+        EquatorialToHorizontalConversion f = new EquatorialToHorizontalConversion(ZonedDateTime.of(2020, 8, 14, 11, 0, 0, 0, ZoneId.of("Europe/Paris")), GeographicCoordinates.ofDeg(2.5, 45));
+        EclipticCoordinates ec1 = EclipticCoordinates.of(Math.PI, Math.PI / 6);
+        assertEquals(HorizontalCoordinates.ofDeg(68.71179, 17.29562).toString(), f.apply(e.apply(ec1)).toString());
+    }
+
+    @Test
     void applyWorksOnKnownValues(){
         EclipticToEquatorialConversion e = new EclipticToEquatorialConversion(ZonedDateTime.of(2009,7,6,0,0,0, 0, ZoneId.of("UTC")));
         assertEquals(EquatorialCoordinates.of(Angle.ofHr(9.581478), Angle.ofDMS(19, 32, 6.01)).toString(), e.apply(EclipticCoordinates.of(Angle.ofDMS(139, 41, 10), Angle.ofDMS(4,52,31))).toString());
--- test/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversionTest.java
+++ test/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversionTest.java
@@ -10,11 +10,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EquatorialToHorizontalConversionTest {
 
+
+
     @Test
     void applyWorksOnKnownValues(){
-        //EquatorialToHorizontalConversion e = new EquatorialToHorizontalConversion(ZonedDateTime.of(2016,7,9,11,31,35, 0, ZoneId.of("Europe/Paris")), GeographicCoordinates.ofDeg(40, 52));
-        //assertEquals(HorizontalCoordinates.of(Angle.ofDMS(283, 16, 15.70), Angle.ofDMS(19,20,3.64)).toString(), e.apply(EquatorialCoordinates.of(Angle.ofDeg(17), Angle.ofDMS(23,13, 10))).toString());
-        EclipticToEquatorialConversion e = new EclipticToEquatorialConversion(ZonedDateTime.of(2009,7,6,0,0,0, 0, ZoneId.of("UTC")));
-        assertEquals(EquatorialCoordinates.of(Angle.ofHr(9.581478), Angle.ofDMS(19, 32, 6.01)).toString(), e.apply(EclipticCoordinates.of(Angle.ofDMS(139, 41, 10), Angle.ofDMS(4,52,31))).toString());
+        //EquatorialToHorizontalConversion e = new EquatorialToHorizontalConversion(ZonedDateTime.of(2020,3,2,17,10,57, 0, ZoneId.of("Europe/Paris")), GeographicCoordinates.ofDeg(2.5, 45));
+        //assertEquals(HorizontalCoordinates.of(0, 0).toString(), e.apply(EquatorialCoordinates.of(Angle.ofHr(2.54), Angle.ofDeg(-23))).toString());
+        //EclipticToEquatorialConversion e = new EclipticToEquatorialConversion(ZonedDateTime.of(2009,7,6,0,0,0, 0, ZoneId.of("UTC")));
+        //assertEquals(EquatorialCoordinates.of(Angle.ofHr(9.581478), Angle.ofDMS(19, 32, 6.01)).toString(), e.apply(EclipticCoordinates.of(Angle.ofDMS(139, 41, 10), Angle.ofDMS(4,52,31))).toString());
+        EquatorialToHorizontalConversion equToHor1= new EquatorialToHorizontalConversion(ZonedDateTime.of(2020,8,14,11,00,00,0, ZoneId.of("Europe/Paris")), GeographicCoordinates.ofDeg(2.5,45));
+        assertEquals(HorizontalCoordinates.ofDeg(65.82367,67.88464).toString(),equToHor1.apply(EquatorialCoordinates.of(Angle.ofHr(8.83764),Angle.ofDeg(49.90546))).toString());
     }
 }
