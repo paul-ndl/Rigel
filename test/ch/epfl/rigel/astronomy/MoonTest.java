3f3abf0782a5d0e871473803ddcb32a807dffede
alexandrebrun <alexandre.brun@epfl.ch>
2020-03-13 14:32:35 +0100

    test sur Planet,Sun & Moon tous finis et bons.

--- test/ch/epfl/rigel/astronomy/MoonTest.java
+++ test/ch/epfl/rigel/astronomy/MoonTest.java
@@ -1,8 +1,10 @@
 package ch.epfl.rigel.astronomy;
 
+import ch.epfl.rigel.coordinates.EclipticCoordinates;
 import ch.epfl.rigel.coordinates.EquatorialCoordinates;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MoonTest {
@@ -16,7 +18,44 @@ public class MoonTest {
             new Moon(EquatorialCoordinates.of(0,0), -0.5f, -17f, 0.5f);
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            new Moon(EquatorialCoordinates.of(0,0), 1f, -17f, -0.5f);
+            new Moon(EquatorialCoordinates.of(0,0), -0.5f, -17f, 2f);
         });
+    }
+
+
+    @Test
+    void nameTest(){
+        Moon p = new Moon(EquatorialCoordinates.of(0,0), 1,2,0.5f);
+        assertEquals("Lune", p.name());
+    }
+
+    @Test
+    void angularSizeTest(){
+        Moon p = new Moon(EquatorialCoordinates.of(0,0), 1,2,0.5f);
+        assertEquals(1, p.angularSize());
+    }
+
+    @Test
+    void magnitudeTest(){
+        Moon p = new Moon(EquatorialCoordinates.of(0,0), 1,2,0.5f);
+        assertEquals(2, p.magnitude());
+    }
+
+    @Test
+    void equatorialPosTest(){
+        Moon p = new Moon(EquatorialCoordinates.of(0,0), 1,2,0.5f);
+        assertEquals(EquatorialCoordinates.of(0,0).toString(), p.equatorialPos().toString());
+    }
+
+    @Test
+    void infoTest(){
+        Moon p = new Moon(EquatorialCoordinates.of(0,0), 1,2,0.5f);
+        assertEquals("Lune (50.0%)", p.info());
+    }
+
+    @Test
+    void toStringTest(){
+        Moon p = new Moon(EquatorialCoordinates.of(0,0), 1,2,0.5f);
+        assertEquals("Lune", p.toString());
     }
 }
