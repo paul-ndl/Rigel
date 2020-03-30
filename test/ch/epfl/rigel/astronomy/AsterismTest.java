01c3c2f7cb0bfc623281243dfcc22f7872216eba
Paul Nadal <paul.nadal@epfl.ch>
2020-03-30 10:57:43 +0200

    Tests étape 5 ajoutés

--- test/ch/epfl/rigel/astronomy/AsterismTest.java
+++ test/ch/epfl/rigel/astronomy/AsterismTest.java
@@ -10,22 +10,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AsterismTest {
-    @Test
-    void isConstructorValidWithValidValues(){
-        Star s= new Star(9, "lol", EquatorialCoordinates.of(0,0),0f,0.5f);
-        List<Star> stars= new ArrayList<>();
-        stars.add(s);
-        List<Star> stars1= new ArrayList<>();
-        stars1.add(s);
-        Asterism a = new Asterism(stars);
-        assertEquals(stars1,a.stars()); }
-
+    private static List<Star> newModifiableStarList(String... starNames) {
+        var eqPos = EquatorialCoordinates.of(0, 0);
+        var stars = new ArrayList<Star>();
+        for (var starName : starNames) {
+            stars.add(new Star(stars.size() + 1, starName, eqPos, 0, 0));
+        }
+        return stars;
+    }
 
     @Test
-    void isConstructorValidWithInValidValues(){
-        List<Star> stars= new ArrayList<>();
+    void constructorFailsWhenStarListIsEmpty() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new Asterism(stars);
+            new Asterism(List.of());
         });
+    }
+
+    @Test
+    void constructorCopiesStarList() {
+        var l = newModifiableStarList("Rigel");
+        var s = l.get(0);
+        var a = new Asterism(l);
+        l.clear();
+        assertEquals(1, a.stars().size());
+        assertEquals(s, a.stars().get(0));
+    }
+
+    @Test
+    void accessorDoesNotAllowEncapsulationViolation() {
+        var a = new Asterism(newModifiableStarList("Rigel", "Aldebaran", "Sirius"));
+        try {
+            a.stars().clear();
+        } catch (UnsupportedOperationException e) {
+            // If UOE is thrown, the list is unmodifiable, which is correct.
+        }
+        assertEquals(3, a.stars().size());
     }
 }
