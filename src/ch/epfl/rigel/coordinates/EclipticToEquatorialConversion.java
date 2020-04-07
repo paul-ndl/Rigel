3b7debf79c47f99a0f67c8f3edaaded15900e1e6
Paul Nadal <paul.nadal@epfl.ch>
2020-04-08 01:03:31 +0200

    vérif package coordinates rendu intermédiaire

--- src/ch/epfl/rigel/coordinates/EclipticToEquatorialConversion.java
+++ src/ch/epfl/rigel/coordinates/EclipticToEquatorialConversion.java
@@ -15,54 +15,59 @@ import java.util.function.Function;
  */
 public final class EclipticToEquatorialConversion implements Function<EclipticCoordinates, EquatorialCoordinates> {
 
-    private final double obliquityCos;
-    private final double obliquitySin;
+    private final double obliquityCos, obliquitySin;
 
     /**
      * Construit une conversion entre les coordonnées écliptiques et équatoriales
-     * @param when
-     *          le couple date/heure de la conversion
+     *
+     * @param when le couple date/heure de la conversion
      */
-    public EclipticToEquatorialConversion (ZonedDateTime when){
+    public EclipticToEquatorialConversion(ZonedDateTime when) {
         final double centuries = Epoch.J2000.julianCenturiesUntil(when);
-        final Polynomial p = Polynomial.of(Angle.ofArcsec(0.00181), -Angle.ofArcsec(0.0006), -Angle.ofArcsec(46.815), Angle.ofDMS(23, 26, 21.45));
-        final double epsilon = p.at(centuries);
-        obliquityCos = Math.cos(epsilon);
-        obliquitySin = Math.sin(epsilon);
+        final Polynomial polynomial = Polynomial.of(Angle.ofArcsec(0.00181),
+                                           -Angle.ofArcsec(0.0006),
+                                           -Angle.ofArcsec(46.815),
+                                           Angle.ofDMS(23, 26, 21.45));
+        final double obliquity = polynomial.at(centuries);
+        obliquityCos = Math.cos(obliquity);
+        obliquitySin = Math.sin(obliquity);
     }
 
     /**
      * Retourne les coordonnées équatoriales à partir des coordonnées écliptiques données
-     * @param ecl
-     *          les coordonnées écliptiques
+     *
+     * @param ecl les coordonnées écliptiques
      * @return les coordonnées équatoriales à partir des coordonnées écliptiques données
      */
     @Override
-    public EquatorialCoordinates apply(EclipticCoordinates ecl){
-        final double lonEcl = ecl.lon();
-        final double latEcl = ecl.lat();
-        final double ra = Angle.normalizePositive(Math.atan2(Math.sin(lonEcl) * obliquityCos - Math.tan(latEcl) * obliquitySin, Math.cos(lonEcl)));
-        final double dec = Math.asin(Math.sin(latEcl) * obliquityCos + Math.cos(latEcl) * obliquitySin * Math.sin(lonEcl));
+    public EquatorialCoordinates apply(EclipticCoordinates ecl) {
+        final double lonSin = Math.sin(ecl.lon());
+        final double latCos = Math.cos(ecl.lat());
+        final double latSin = Math.sin(ecl.lat());
+        final double latTan = Math.tan(ecl.lat());
+        final double ra = Angle.normalizePositive(Math.atan2(lonSin*obliquityCos - latTan*obliquitySin, latCos));
+        final double dec = Math.asin(latSin*obliquityCos + latCos*obliquitySin*latSin);
         return EquatorialCoordinates.of(ra, dec);
     }
 
     /**
      * Empêche d'utiliser cette méthode
-     * @param o
-     *          un objet
+     *
+     * @param o un objet
      * @throws UnsupportedOperationException
      */
     @Override
-    public final boolean equals(Object o){
+    public final boolean equals(Object o) {
         throw new UnsupportedOperationException();
     }
 
     /**
      * Empêche d'utiliser cette méthode
+     *
      * @throws UnsupportedOperationException
      */
     @Override
-    public final int hashCode(){
+    public final int hashCode() {
         throw new UnsupportedOperationException();
     }
 
