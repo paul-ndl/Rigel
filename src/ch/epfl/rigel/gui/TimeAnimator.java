d277cbd34271720c8c11085dc04cdce690973f8b
Paul Nadal <paul.nadal@epfl.ch>
2020-04-21 17:16:50 +0200

    étape 9 timeAnimator

--- src/ch/epfl/rigel/gui/TimeAnimator.java
+++ src/ch/epfl/rigel/gui/TimeAnimator.java
@@ -6,11 +6,16 @@ import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
+import java.sql.SQLOutput;
+import java.time.ZonedDateTime;
+
 public final class TimeAnimator extends AnimationTimer {
 
     private DateTimeBean dateTimeBean;
     private SimpleObjectProperty<TimeAccelerator> accelerator = new SimpleObjectProperty<>(null);
     private SimpleBooleanProperty running = new SimpleBooleanProperty(false);
+    private long time0;
+    private long deltaTime = 0;
 
     public TimeAnimator(DateTimeBean dateTimeBean){
         this.dateTimeBean = dateTimeBean;
@@ -42,13 +47,17 @@ public final class TimeAnimator extends AnimationTimer {
 
     @Override
     public void handle(long now) {
-        getAccelerator().adjust(dateTimeBean.getZonedDateTime(), now);
+        long actualTime = now;
+        deltaTime = actualTime - time0 - deltaTime;
+        System.out.println(deltaTime);
+        dateTimeBean.setZonedDateTime(getAccelerator().adjust(dateTimeBean.getZonedDateTime(), deltaTime));
     }
 
     @Override
     public void start(){
         super.start();
         setRunning(true);
+        time0 = System.nanoTime();
     }
 
     @Override
