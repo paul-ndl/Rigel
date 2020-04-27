0733d2d1c40926365ca42417ac2745ba31c4c601
Paul Nadal <paul.nadal@epfl.ch>
2020-04-27 11:04:28 +0200

    étape 10 start

---
+++ src/ch/epfl/rigel/gui/ObserverLocationBean.java
@@ -0,0 +1,44 @@
+package ch.epfl.rigel.gui;
+
+import ch.epfl.rigel.coordinates.GeographicCoordinates;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
+
+public final class ObserverLocationBean {
+
+    private final DoubleProperty lonDeg = new SimpleDoubleProperty();
+    private final DoubleProperty latDeg = new SimpleDoubleProperty();
+    private final ObservableValue<GeographicCoordinates> coordinates = Bindings.createObjectBinding(
+            () -> GeographicCoordinates.ofDeg(lonDeg.get(), latDeg.get())
+    );
+
+    public DoubleProperty lonDegProperty(){
+        return lonDeg;
+    }
+
+    public Double getLonDeg(){
+        return lonDeg.get();
+    }
+
+    public void setLonDeg(Double lonDeg){
+        this.lonDeg.set(lonDeg);
+    }
+
+    public DoubleProperty latDegProperty(){
+        return latDeg;
+    }
+
+    public Double getLatDeg(){
+        return latDeg.get();
+    }
+
+    public void setLatDeg(Double latDeg){
+        this.latDeg.set(latDeg);
+    }
+
+
+}
