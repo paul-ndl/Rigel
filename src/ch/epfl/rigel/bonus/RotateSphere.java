1b8b4b14637914c44ad0afe77601fb2ddb18f0a7
Paul Nadal <paul.nadal@epfl.ch>
2020-05-08 20:09:39 +0200

    start bonus

--- src/ch/epfl/rigel/bonus/RotateSphere.java
+++ src/ch/epfl/rigel/bonus/RotateSphere.java
@@ -16,6 +16,8 @@ import javafx.stage.Stage;
 
 public class RotateSphere extends Application {
 
+    private boolean playing = true;
+
     public static void main(String[] args) { launch(args); }
 
     @Override
@@ -34,35 +36,48 @@ public class RotateSphere extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
     private Sphere world(Stage primaryStage){
         Sphere world = new Sphere();
+
         world.radiusProperty().bind(Bindings.createDoubleBinding(() ->
                         primaryStage.getWidth()>primaryStage.getHeight() ? primaryStage.getHeight()*0.35 : primaryStage.getWidth()*0.35,
                 primaryStage.widthProperty(), primaryStage.heightProperty()
         ));
+
         world.translateXProperty().bind(Bindings.createDoubleBinding(() ->
                 primaryStage.getWidth()/2, primaryStage.widthProperty()));
         world.translateYProperty().bind(Bindings.createDoubleBinding(() ->
                 primaryStage.getHeight()/2, primaryStage.heightProperty()));
+
         PhongMaterial texture = new PhongMaterial();
         texture.setDiffuseMap(new Image(getClass().getResource("/earth_texture.png").toExternalForm()));
         world.setMaterial(texture);
 
         world.setRotationAxis(Rotate.Y_AXIS);
         primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, eventHandler -> {
-            switch (eventHandler.getCode()) {
-                //increase the world size if we press A
-                case RIGHT:
-                    world.rotateProperty().set(world.getRotate()+15);
-                    break;
-                //decrease the world size if we press B
-                case LEFT:
-                    world.rotateProperty().set(world.getRotate()-15);
-                    break;
+            if(playing){
+                switch (eventHandler.getCode()) {
+                    //increase the world size if we press A
+                    case RIGHT:
+                        world.rotateProperty().set(world.getRotate()+15);
+                        break;
+                    //decrease the world size if we press B
+                    case LEFT:
+                        world.rotateProperty().set(world.getRotate()-15);
+                        break;
+                }
             }
         });
         return world;
+    }
+
+    private void animation(Sphere world) throws InterruptedException {
+        for(int i=0; i<36; ++i){
+            world.rotateProperty().set(world.getRotate()+10);
+            Thread.sleep(100);
+        }
     }
 }
