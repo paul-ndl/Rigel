299a5b426c7936f2c0455dcc36dea627df01913b
Paul Nadal <paul.nadal@epfl.ch>
2020-05-28 21:43:16 +0200

    commentaires code rendu final

--- src/ch/epfl/rigel/gui/ViewingParametersBean.java
+++ src/ch/epfl/rigel/gui/ViewingParametersBean.java
@@ -6,33 +6,68 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
-
+/**
+ * Un bean contenant les paramètres déterminant la portion du ciel visible
+ *
+ * @author Paul Nadal (300843)
+ * @author Alexandre Brun (302477)
+ */
 public final class ViewingParametersBean {
 
     private final DoubleProperty fieldOfViewDeg = new SimpleDoubleProperty();
     private final ObjectProperty<HorizontalCoordinates> center = new SimpleObjectProperty<>(null);
 
-    public DoubleProperty fieldOfViewDegProperty(){
+    /**
+     * Retourne la propriété du champ de vue
+     *
+     * @return la propriété du champ de vue
+     */
+    public DoubleProperty fieldOfViewDegProperty() {
         return fieldOfViewDeg;
     }
 
-    public Double getFieldOfViewDeg(){
+    /**
+     * Retourne le champ de vue
+     *
+     * @return le champ de vue
+     */
+    public Double getFieldOfViewDeg() {
         return fieldOfViewDeg.get();
     }
 
-    public void setFieldOfViewDeg(Double fieldOfViewDeg){
+    /**
+     * Paramètre le champ de vue à celui donné
+     *
+     * @param fieldOfViewDeg le nouveau champ de vue
+     */
+    public void setFieldOfViewDeg(Double fieldOfViewDeg) {
         this.fieldOfViewDeg.set(fieldOfViewDeg);
     }
 
-    public ObjectProperty<HorizontalCoordinates> centerProperty(){
+    /**
+     * Retourne la propriété du centre de vue
+     *
+     * @return la propriété du centre de vue
+     */
+    public ObjectProperty<HorizontalCoordinates> centerProperty() {
         return center;
     }
 
-    public HorizontalCoordinates getCenter(){
+    /**
+     * Retourne le centre de vue
+     *
+     * @return le centre de vue
+     */
+    public HorizontalCoordinates getCenter() {
         return center.get();
     }
 
-    public void setCenter(HorizontalCoordinates center){
+    /**
+     * Paramètre le centre de vue à celui donné
+     *
+     * @param center le nouveau centre de vue
+     */
+    public void setCenter(HorizontalCoordinates center) {
         this.center.set(center);
     }
 }
